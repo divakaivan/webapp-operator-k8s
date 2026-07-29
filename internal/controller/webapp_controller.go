05491081297ec5c699d1ce9ebd29d26ec34df9ec
divakaivan <timee98@outlook.com>
2026-07-29 23:21:44 +0100

    add deploymentFor

--- internal/controller/webapp_controller.go
+++ internal/controller/webapp_controller.go
@@ -25,6 +25,9 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	webappv1 "github.com/divakaivan/webapp-operator-k8s/api/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // WebAppReconciler reconciles a WebApp object
@@ -56,6 +59,34 @@ func (r *WebAppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 	log.Info("fetched WebApp", "image", webapp.Spec.Image, "replicas", webapp.Spec.Replicas)
 
 	return ctrl.Result{}, nil
+}
+
+func deploymentFor(w *webappv1.WebApp) *appsv1.Deployment {
+	labels := map[string]string{"app": w.Name}
+	replicas := w.Spec.Replicas
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      w.Name,
+			Namespace: w.Namespace,
+			Labels:    labels,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{MatchLabels: labels},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{Labels: labels},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Name:  "web",
+						Image: w.Spec.Image,
+						Ports: []corev1.ContainerPort{{
+							ContainerPort: 80,
+						}},
+					}},
+				},
+			},
+		},
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
